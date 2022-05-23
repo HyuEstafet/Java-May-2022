f6f10021b65d51ee002e5e701393d30a12faebfd
HyuEstafet <hyulya.gyuler@estafet.com>
2022-05-23 22:37:24 +0300

    committing some String examples

--- src/Main_06.java
+++ src/Main_06.java
@@ -1,2 +1,15 @@
 public class Main_06 {
+
+    public static void main(String[] args) {
+        String a = "Hello";
+        System.out.println(a + " to our home");
+
+        String word1 = "Java ";
+        String word2 = "is ";
+        String word3 = "tricky ";
+
+        String sum = word1 + word2 + word3;
+        System.out.println("Today's quote: " + sum);
+    }
 }
+
